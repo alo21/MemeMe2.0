99176720610e23e80783dce1ec2c91ece7104289
Alessandro Losavio <alessandro.losavio@mail.polimi.it>
2019-01-10 22:38:21 +0100

    Initial Commit

---
+++ MemeMe2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe2.0
+//
+//  Created by Alessandro Losavio on 1/10/19.
+//  Copyright © 2019 Losavio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
